6395c1a4b0949de1510cf13b0cccb99660d614f9
CarlosCortes2020 <cicc81@gmail.com>
2024-06-20 23:19:33 -0600

    commit con archivos de la clase 11

---
+++ 011_logic.py
@@ -0,0 +1,23 @@
+# and
+print("<= AND =>")
+print("True and True =>", True and True)
+print("True and False =>", True and False)
+
+print(10 > 5 and 5 < 10)
+
+stock = input("Ingrese el numero de stock => ")
+stock = int(stock)
+
+print(stock >= 100 and stock <= 1000)
+
+
+# or
+print("<= OR =>")
+print("True or True =>", True or True)
+print("True or False =>", True or False)
+
+print(10 > 5 or 5 < 10)
+
+rol = input("Digite el rol => ")
+
+print(rol == "admin" or rol == "seller")
