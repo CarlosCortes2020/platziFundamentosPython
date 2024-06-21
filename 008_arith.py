5ad1e8cf435e60241231ff113694dd36f9f6ee01
CarlosCortes2020 <cicc81@gmail.com>
2024-06-20 21:35:09 -0600

    Con archivo 8 previo a 9 de comparadores

---
+++ 008_arith.py
@@ -0,0 +1,13 @@
+print(10 + 20)
+print(10 - 5)
+print(10 * 2)
+print(10 / 2)
+print(10 % 3)
+
+print("Mira " + "como se me estira")
+print("Holoooo" * 3)
+
+print(10 // 3)
+print(2 ** 3)
+
+print(2 ** 3 + 3 - 7 / 1 // 4)
