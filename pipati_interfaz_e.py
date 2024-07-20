780c2704bd86f54b057bd25984050c925ecf55c3
CarlosCortes2020 <cicc81@gmail.com>
2024-07-20 10:41:50 -0600

    Primer version de piedra pael tijera con imagenes, previo a eliminar mensaje d esobr

--- pipati_interfaz_e.py
+++ pipati_interfaz_e.py
@@ -16,16 +16,21 @@ class RockPaperScissors:
         self.canvas = tk.Canvas(root, width=600, height=500, bg='white')
         self.canvas.pack(pady=20)  # Coloca el lienzo en la ventana principal con un margen superior de 20 píxeles
 
+        # Cargar imágenes
+        self.piedra_img = tk.PhotoImage(file="./rock.png")
+        self.papel_img = tk.PhotoImage(file="./papel.png")
+        self.tijera_img = tk.PhotoImage(file="./tijera.png")
+
         # Crea el botón "Piedra" y lo agrega al marco de los botones
-        self.rock_button = tk.Button(self.button_frame, text="Piedra", command=self.rock, width=10, height=2)
+        self.rock_button = tk.Button(self.button_frame, image=self.piedra_img, command=self.rock, width=50, height=50)
         self.rock_button.pack(side=tk.LEFT, padx=10, pady=10)  # Coloca el botón a la izquierda con márgenes horizontales y verticales de 10 píxeles
 
         # Crea el botón "Papel" y lo agrega al marco de los botones
-        self.paper_button = tk.Button(self.button_frame, text="Papel", command=self.paper, width=10, height=2)
+        self.paper_button = tk.Button(self.button_frame, image=self.papel_img, command=self.paper, width=50, height=50)
         self.paper_button.pack(side=tk.LEFT, padx=10, pady=10)  # Coloca el botón a la izquierda con márgenes horizontales y verticales de 10 píxeles
 
         # Crea el botón "Tijera" y lo agrega al marco de los botones
-        self.scissors_button = tk.Button(self.button_frame, text="Tijera", command=self.scissors, width=10, height=2)
+        self.scissors_button = tk.Button(self.button_frame, image=self.tijera_img, command=self.scissors, width=50, height=50)
         self.scissors_button.pack(side=tk.LEFT, padx=10, pady=10)  # Coloca el botón a la izquierda con márgenes horizontales y verticales de 10 píxeles
 
     def rock(self):
@@ -33,21 +38,21 @@ class RockPaperScissors:
         user_choice = "Piedra"
         computer_choice = self.random_choice()  # Genera la elección de la computadora
         result = self.determine_winner(user_choice, computer_choice)
-        self.display_message(f"Elegiste {user_choice}.", f"Computadora eligió {computer_choice}.", f"{result}")
+        self.display_message(user_choice, computer_choice, f"Elegiste {user_choice}.", f"Computadora eligió {computer_choice}.", f"{result}")
 
     def paper(self):
         # Acción cuando se hace clic en el botón "Papel"
         user_choice = "Papel"
         computer_choice = self.random_choice()  # Genera la elección de la computadora
         result = self.determine_winner(user_choice, computer_choice)
-        self.display_message(f"Elegiste {user_choice}.", f"Computadora eligió {computer_choice}.", f"{result}")
+        self.display_message(user_choice, computer_choice, f"Elegiste {user_choice}.", f"Computadora eligió {computer_choice}.", f"{result}")
 
     def scissors(self):
         # Acción cuando se hace clic en el botón "Tijera"
         user_choice = "Tijera"
         computer_choice = self.random_choice()  # Genera la elección de la computadora
         result = self.determine_winner(user_choice, computer_choice)
-        self.display_message(f"Elegiste {user_choice}.", f"Computadora eligió {computer_choice}.", f"{result}")
+        self.display_message(user_choice, computer_choice, f"Elegiste {user_choice}.", f"Computadora eligió {computer_choice}.", f"{result}")
 
     def random_choice(self):
         # Genera una elección aleatoria entre "Piedra", "Papel" y "Tijera"
@@ -65,13 +70,35 @@ class RockPaperScissors:
         else:
             return "¡Perdiste!"
 
-    def display_message(self, user_message, computer_message, winner_message):
+    def display_message(self, user_choice, computer_choice, user_message, computer_message, winner_message):
         # Limpia el lienzo antes de mostrar un nuevo mensaje
         self.canvas.delete("all")
+         # Mostrar imágenes de las elecciones
+        if user_choice == "Piedra":
+            user_img = self.piedra_img
+        elif user_choice == "Papel":
+            user_img = self.papel_img
+        else:
+            user_img = self.tijera_img
+
+        if computer_choice == "Piedra":
+            computer_img = self.piedra_img
+        elif computer_choice == "Papel":
+            computer_img = self.papel_img
+        else:
+            computer_img = self.tijera_img
+
+        #Etiquetas de jugador
+        self.canvas.create_text(200, 50, text="Player",font=("Arial", 24), fill="black")
+        self.canvas.create_text(400, 50, text="Computer",font=("Arial", 24), fill="black")
+        # Mostrar las imágenes en el canvas
+        self.canvas.create_image(200, 100, image=user_img)
+        self.canvas.create_image(400, 100, image=computer_img)
         # Muestra los mensajes en el centro del lienzo en dos líneas diferentes
-        self.canvas.create_text(300, 220, text=user_message, font=("Arial", 24), fill="black")
-        self.canvas.create_text(300, 280, text=computer_message, font=("Arial", 24), fill="black")
-        self.canvas.create_text(300, 320, text=winner_message, font=("Arial", 24), fill="black")
+        self.canvas.create_text(300, 200, text=user_message, font=("Arial", 24), fill="black")
+        self.canvas.create_text(300, 250, text=computer_message, font=("Arial", 24), fill="black")
+        self.canvas.create_text(300, 300, text=winner_message, font=("Arial", 24), fill="black")
+
 
 if __name__ == "__main__":
     root = tk.Tk()  # Crea la ventana principal de Tkinter
